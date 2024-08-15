c2be7c7d4a62f7386f893d81e832a03198e3d5c1
aislanlima13 <aislan.martins99@gmail.com>
2024-08-15 11:18:08 -0300

    Adds improvements to favorite functionality and an interactive menu

--- basicos/memoteca_no_standalone/src/app/app-routing.module.ts
+++ basicos/memoteca_no_standalone/src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { RouterModule, Routes } from '@angular/router';
+import { RouteReuseStrategy, RouterModule, Routes } from '@angular/router';
 import { CriarPensamentoComponent } from './componentes/pensamentos/criar-pensamento/criar-pensamento.component';
 import { ListarPensamentosComponent } from './componentes/pensamentos/listar-pensamentos/listar-pensamentos.component';
 import { ExcluirPensamentoComponent } from './componentes/pensamentos/excluir-pensamento/excluir-pensamento.component';
 import { EditarPensamentoComponent } from './componentes/pensamentos/editar-pensamento/editar-pensamento.component';
+import { CustomReuseStrategy } from './custom-reuse-estrategy';
 
 const routes: Routes = [
   {
@@ -14,7 +15,11 @@ const routes: Routes = [
     path: 'criarPensamento', component: CriarPensamentoComponent
   },
   {
-    path: 'listarPensamento', component: ListarPensamentosComponent
+    path: 'listarPensamento',
+    component: ListarPensamentosComponent,
+    data: {
+      reuseComponent: true
+    }
   },
   {
     path: 'pensamentos/excluirPensamento/:id', component: ExcluirPensamentoComponent
@@ -26,7 +31,10 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule, FormsModule]
+  imports: [RouterModule.forRoot(routes, {onSameUrlNavigation: 'reload'})],
+  exports: [RouterModule, FormsModule],
+  providers: [
+    {provide: RouteReuseStrategy, useClass: CustomReuseStrategy}
+  ]
 })
 export class AppRoutingModule { }
